7bbbf2135bc03eab234ea823e7f72a8bb01efd9f
wonder <samuel.nai@yahoo.com>
2023-02-15 01:00:16 +0000

    add dummy data

---
+++ src/homeData.js
@@ -0,0 +1,70 @@
+import prop1 from './assets/prop1.png'
+import prop2 from './assets/prop2.png'
+import prop3 from './assets/prop3.png'
+import prop4 from './assets/prop4.png'
+import prop5 from './assets/prop5.png'
+import prop6 from './assets/prop6.png'
+import flex1 from './assets/flex1.png'
+import flex2 from './assets/flex2.png'
+import flex3 from './assets/flex3.png'
+import flex4 from './assets/flex4.png'
+
+
+export const properties = [
+    {
+        loc: "2578 Folsom street, san francisco, CA, 94110",
+        type: "Private Room",
+        price: "$1200/month",
+        img: prop1
+    },
+    {
+        loc: "2578 Folsom street, san francisco, CA, 94110",
+        type: "Private Room",
+        price: "$1200/month",
+        img: prop2
+    },
+    {
+        loc: "2578 Folsom street, san francisco, CA, 94110",
+        type: "Private Room",
+        price: "$1200/month",
+        img: prop3
+    },
+    {
+        loc: "2578 Folsom street, san francisco, CA, 94110",
+        type: "Private Room",
+        price: "$1200/month",
+        img: prop4
+    },
+    {
+        loc: "2578 Folsom street, san francisco, CA, 94110",
+        type: "Private Room",
+        price: "$1200/month",
+        img: prop5
+    },
+    {
+        loc: "2578 Folsom street, san francisco, CA, 94110",
+        type: "Private Room",
+        price: "$1200/month",
+        img: prop6
+    },
+
+]
+
+export const flexible = [
+    {
+        title: "Flexible Lenses",
+        img: flex1,
+    },
+    {
+        title: "7-Day Happiness Guaranteed",
+        img: flex2,
+    },
+    {
+        title: "Monthly House Cleaning",
+        img: flex3,
+    },
+    {
+        title: "Choose Your Own Roommate",
+        img: flex4,
+    },
+]
