f7b2095176f5a90d51e28712d30c9f364cf56859
wonder <samuel.nai@yahoo.com>
2023-02-15 00:59:50 +0000

    configure router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, redirect } from 'react-router-dom';
+import Home from './Home';
+import Header from './Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header />,
+    
+    children: [
+      {
+        index: true,
+        element: <Home />,
+        loader: ()=>redirect('/Home'),
+      },
+      {
+        path: "Home",
+        element: <Home />,
+      },
+      {
+        path: "LandLord",
+        element: undefined,
+      },
+      {
+        path: "Tenants",
+        element: undefined,
+      },
+      {
+        path: "ContactUs",
+        element: undefined,
+      },
+    ]
+  }
+])
 
-export default App;
+
+export default router;
