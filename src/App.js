ec9bfdf6e5cefe7bebed9005aef235c45441c772
wonder <samuel.nai@yahoo.com>
2023-04-02 21:41:08 +0000

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 import { createBrowserRouter, redirect } from 'react-router-dom';
 import Home from './Home';
 import Header from './Header';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Header />,
+      
+      children: [
+        {
+          index: true,
+          element: <Home />,
+          loader: ()=>redirect('/Home'),
+        },
+        {
+          path: "/Home",
+          element: <Home />,
+        },
+        {
+          path: "/LandLord",
+          element: undefined,
+        },
+        {
+          path: "/Tenants",
+          element: undefined,
+        },
+        {
+          path: "/ContactUs",
+          element: undefined,
+        },
+      ]
+    },
+  ],
+
   {
-    path: "/",
-    element: <Header />,
-    
-    children: [
-      {
-        index: true,
-        element: <Home />,
-        loader: ()=>redirect('/Home'),
-      },
-      {
-        path: "Home",
-        element: <Home />,
-      },
-      {
-        path: "LandLord",
-        element: undefined,
-      },
-      {
-        path: "Tenants",
-        element: undefined,
-      },
-      {
-        path: "ContactUs",
-        element: undefined,
-      },
-    ]
+    basename: '/home_rentals'
   }
-])
+)
 
 
 export default router;
